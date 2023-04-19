043b9b86a1a303fad2caca9db362c0becec25cdb
oscmor8 <oscarmoradoo@gmail.com>
2023-04-19 12:56:55 -0400

    Updates

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ console.log(`The name ${name1} is longer than ${name2} by ${nameDiff}`)
 // MEDIUM: Write a program to tell if someone is shouting (typing in all caps), whispering (typing in all lowercase), or neither. Use prompt to get user input, and then console log whether the user was shouting, whispering, or talking normally.
 
 
-
+let input = prompt("Type your message: ")
 function formFunc(){
     if (input === input.toUpperCase()){
         console.log("Stop Shouting!")
@@ -27,10 +27,11 @@ function formFunc(){
     console.log("Thank you for talking normally")
 }
 
-let input = prompt("Type your message: ")
 let result = formFunc (input)
-console.log(result)
+
 // HARD: Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
+
+
 
 // VERY HARD: Create a simple calculator that prompts the user for a number, an operator (either +, -, * or /) and another number, and then uses the functions created in the hard challenge to output the value in sentence form. Example output: "3 + 4 = 7"
 
